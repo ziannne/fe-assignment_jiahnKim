6b4391d3138bf6d1063fb10f795c16a139a5a970
Kim <rlawldks7788@gmail.com>
2026-05-27 17:45:34 +0900

    6주차과제

---
+++ week6/src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router";
+
+
+import App from './App.jsx'
+
+import "./index.css";
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
